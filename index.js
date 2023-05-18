88db4ff3e8518a27212677f4d3d715c3fd243c63
RahulYactraq <kadamrahulj0909@gmail.com>
2023-05-19 02:55:51 +0530

    added post endpoint

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 const express = require('express')
 const app = express()
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
 app.get('/', (req, res) => {
+
+    console.log("You hit the root api request");
     return res.status(200).json({
         message: "Hello this api endpoint is for apifor.email website",
     });
 });
+
+
+app.post('/email-verification', (req, res) => {
+    const { text } = req.body;
+
+    console.log(text);
+
+    return res.status(200).json({
+        message: "Email recevied for verification", text: text
+    });
+});
+
 app.listen(process.env.PORT || 3000)
