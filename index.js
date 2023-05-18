5fc417ef66d553fda4702689ec772fe7b1c51d01
RahulYactraq <kadamrahulj0909@gmail.com>
2023-05-19 02:13:41 +0530

    update root path to show info about what this api endpoint is about

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 app.get('/', (req, res) => {
     return res.status(200).json({
-        message: "Hello from root!",
+        message: "Hello this api endpoint is for apifor.email website",
     });
 });
 app.listen(process.env.PORT || 3000)
