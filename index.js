fab1bf48ed2dc5b6a7d7475415ede5262bf50fb8
RahulYactraq <kadamrahulj0909@gmail.com>
2023-05-19 03:10:53 +0530

    Email verification Added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const validator = require('deep-email-validator');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -14,14 +15,20 @@ app.get('/', (req, res) => {
 });
 
 
-app.post('/email-verification', (req, res) => {
+app.post('/email-verification', async (req, res) => {
     const { email } = req.body;
 
     console.log(email);
 
-    return res.status(200).json({
-        message: "Email recevied for verification", email: email
-    });
+      // Validate the email address
+    const { valid, reason } = await validator.validate(email);
+
+    if (!valid) {
+        return res.status(400).json({ message: 'Invalid email address.', reason: reason });
+    }
+    
+      // Return the verified email response
+    return res.status(200).json({ message: 'Email address verified successfully.', email: email, valid: valid });
 });
 
 app.listen(process.env.PORT || 3000)
