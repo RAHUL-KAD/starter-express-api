12d34c2cb9fda5f10cc3425cf70dbf86bc4125a7
RAHUl-KAD <kadamrahulj0909@gmail.com>
2023-07-13 08:41:32 +0530

    added support for user id verification and also added code to remove the caching from each website

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const geoip = require('geoip-lite');
 const { createClient } = require('@supabase/supabase-js');
 const base64 = require('base64-js');
 const { DateTime } = require('luxon');
-
 const SUPABASE_URL = 'https://xwskviprdexmphfivdtm.supabase.co';
-const SUPABASE_KEY = process.env.SUPABASE_KEY; // Add your Supabase service_role API key
+const SUPABASE_KEY = process.env.SUPABASE_KEY;
 const SUPABASE_TABLE = 'pixel-tracker';
+const SUPABASE_USERS_TABLE = 'clerk-users';
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
@@ -19,15 +19,69 @@ app.get('/status', (req, res) => {
   res.send('ok!');
 });
 
-app.get('/pixel.gif', (req, res) => {
+function extractUniqueUserId(user_id) {
+  return supabase
+    .from(SUPABASE_USERS_TABLE)
+    .select('unique_user_id')
+    .then(({ data }) => {
+      console.log(data);
+
+      // Check if error occurred while retrieving data
+      if (data === null) {
+        console.log('Error occurred while retrieving data.');
+        return null;
+      }
+
+      // Equivalent for SQL Query "SELECT * FROM games;"
+      console.log(data);
+      for (const item of data) {
+        if ('unique_user_id' in item && item['unique_user_id'] === user_id) {
+          console.log('unique user id exists');
+          return true;
+        }
+      }
+      console.log('unique user id does not exist');
+      return false;
+    })
+    .catch((error) => {
+      console.error('An error occurred:', error);
+      return null;
+    });
+}
+
+app.get('/pixel.gif', async (req, res) => {
   const origin_page = req.query.page || 'main';
   const client_ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress || '';
   const referrer = req.headers.referer || '';
   const geo_data = getGeoData(client_ip);
   const user_agent = req.headers['user-agent'] || '';
   const device_type = getDeviceType(user_agent);
+  let user_id = '';
+
+  for (const [key, value] of Object.entries(req.query)) {
+    console.log(`${key}: ${value}`);
+  }
+
+  if ('user-id' in req.query) {
+    user_id = req.query['user-id'];
+
+    // checking if user id exists or not in Supabase and then only we will add other details
+    const check_user_id = await extractUniqueUserId(user_id);
+    if (check_user_id) {
+      // visitor IP
+      // (Rest of the code remains the same)
+      console.log("user id is verified");
+    } else {
+      user_id = "User id doesn't exist";
+    }
+  }
 
   const pixel_data = base64.toByteArray('R0lGODlhAQABAIAAAP8AAP8AACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==');
+
+  res.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate max-age=0');
+  res.set('Pragma', 'no-cache');
+  res.set('Expires', '0');
+  res.set('Surrogate-Control', 'no-store');
 
   const st = DateTime.now().toFormat('yyyy-MM-dd HH:mm:ss');
   const ist_now = DateTime.now().setZone('Asia/Kolkata').toFormat('yyyy-MM-dd HH:mm:ss.SSS');
@@ -41,6 +95,7 @@ app.get('/pixel.gif', (req, res) => {
     referrer,
     geo: geo_data,
     device_type,
+    unique_user_id: user_id
   };
 
   supabase
@@ -55,6 +110,8 @@ app.get('/pixel.gif', (req, res) => {
       res.sendStatus(500);
     });
 });
+
+
 
 function getGeoData(ip) {
   const geo = geoip.lookup(ip);
